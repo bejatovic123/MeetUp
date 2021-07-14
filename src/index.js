6dcba4e03fe48fa34579ba5ea05470c37a39d6a9
Bratislav Bejatovic <bratislavbeat@gmail.com>
2021-07-14 19:59:35 +0200

    End of section 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App.jsx';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(<App />, rootEl);
+}
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App.jsx', function () {
+    setTimeout(render);
+  });
+}
+render();
+
+//Hot Module Replacement (or HMR) is one of the most useful features offered by webpack. It allows all kinds of modules to be updated at runtime without the need for a full refresh
+//refresuje sve promjene bez refresha u ovom slucaju App.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
