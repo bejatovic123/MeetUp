a702c2c01282d65259f41f9f92a6fd4dabff4bca
Bratislav Bejatovic <bratislavbeat@gmail.com>
2021-10-25 20:32:57 +0200

    update 1

--- src/index.js
+++ src/index.js
@@ -34,10 +34,6 @@ if (module.hot) {
 }
 render();
 
-//Hot Module Replacement (or HMR) is one of the most useful features offered by webpack. It allows all kinds of modules to be updated at runtime without the need for a full refresh
 //refresuje sve promjene bez refresha u ovom slucaju App.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
